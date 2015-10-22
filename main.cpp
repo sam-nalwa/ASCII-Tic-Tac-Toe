61207b51cc31f9f52f102494438dcda9c41085d1
Sam Nalwa <sam@nalwa.ca>
2015-10-22 09:27:03 -0400

    Getting a hang of C++

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
@@ -7,4 +8,17 @@ char grid[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
 int checkwin();
 void board();
+
+void Print(int myInt)
+{
+    printf("My int is %d", myInt);
+}
+
+int main()
+{
+    printf("Hello, world!\n");
+    Print(20);
+    return 0;
+}
+
 
